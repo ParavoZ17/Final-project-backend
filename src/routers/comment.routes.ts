fcb41be94a079eafac6b08dd30e67592115d987a
Mykola <1712nn2012@gmail.com>
2025-12-14 21:27:29 +0100

    fix cloudinary version

--- src/routers/comment.routes.ts
+++ src/routers/comment.routes.ts
@@ -9,13 +9,11 @@ import {
 
 const commentsRouter = Router({ mergeParams: true });
 
-// POST /comments
+
 commentsRouter.post("/", authenticate, asyncHandler(addCommentController));
 
-// GET /comments
 commentsRouter.get("/", authenticate, asyncHandler(getCommentsController));
 
-// DELETE /comments/:commentId
 commentsRouter.delete("/:commentId", authenticate, asyncHandler(deleteCommentController));
 
 export default commentsRouter;
